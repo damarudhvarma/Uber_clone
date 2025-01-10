bf766d87d1a959573a2c32a82e60ba30ac0c7d64
damarudhvarma <damarudhvarma.dandu@gmail.com>
2025-01-10 21:55:38 +0530

    Scoket ID upadted in schema

--- server/models/captainModel.js
+++ server/models/captainModel.js
@@ -24,7 +24,7 @@ const captainSchema = new mongoose.Schema({
             required:true,
             select:false,
         },
-        socketid:{
+        socketId:{
             type:String
         },
         status:{
--- server/models/userModule.js
+++ server/models/userModule.js
@@ -24,7 +24,7 @@ const userSchema= new mongoose.Schema({
         required:true,
         select:false,
     },
-    socketid:{
+    socketId:{
         type:String
     },
    
